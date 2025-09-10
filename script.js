99fa1a69069b2246ac25d563c93575997d6a158f
RizkySanDex <rizky2me@gmail.com>
2025-09-10 18:38:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const videoSource = mainVideo.querySelector('source');
 const videoTitle = document.getElementById('videoTitle');
 const playlistEl = document.getElementById('playlist');
 
+// Generate thumbnail otomatis dari frame video ke-2
+function generateThumbnail(videoFile, callback, seekTime = 2) {
+    const tempVideo = document.createElement('video');
+    tempVideo.src = videoFile;
+    tempVideo.crossOrigin = 'anonymous';
+    tempVideo.muted = true;
+
+    tempVideo.addEventListener('loadeddata', () => { tempVideo.currentTime = seekTime; });
+    tempVideo.addEventListener('seeked', () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = tempVideo.videoWidth;
+        canvas.height = tempVideo.videoHeight;
+        canvas.getContext('2d').drawImage(tempVideo, 0, 0, canvas.width, canvas.height);
+        callback(canvas.toDataURL('image/png'));
+    });
+}
+
 fetch('videos.json')
   .then(res => res.json())
   .then(videos => {
@@ -11,7 +28,7 @@ fetch('videos.json')
       li.textContent = video.title;
       li.addEventListener('click', () => {
         videoSource.src = video.file;
-        mainVideo.poster = video.thumbnail || "";
+        generateThumbnail(video.file, thumb => mainVideo.poster = thumb);
         mainVideo.load();
         mainVideo.play().catch(() => {});
         videoTitle.textContent = video.title;
@@ -22,7 +39,7 @@ fetch('videos.json')
     // Load video pertama otomatis
     if(videos.length > 0){
       videoSource.src = videos[0].file;
-      mainVideo.poster = videos[0].thumbnail || "";
+      generateThumbnail(videos[0].file, thumb => mainVideo.poster = thumb);
       mainVideo.load();
       videoTitle.textContent = videos[0].title;
     }
